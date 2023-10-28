1312460b7090089ced76c5fc4af528e92a785041
Cyprien Amigon <cyp.amigon@gmail.com>
2023-10-28 11:09:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,89 @@
+# inat_downloader
+
+inat_downloader is a Python script for downloading images and metadata from [iNaturalist.org](https://www.inaturalist.org/ "iNaturalist's Homepage") observations.  
+
+## Installation
+
+
+1. Install the required Python package : 
+```
+pip install requests
+```
+2. Clone the repository : 
+```
+git clone https://github.com/cypamigon/inat_downloader.git
+```
+3. Change your current working directory to the downloaded folder : 
+```
+cd inat_downloader
+```
+
+## Usage 
+
+1. Prepare a CSV file named `species.csv` containing the scientific name of the species for which you want to download images and metadata. You also need to associate a start ID for each species. The CSV file must have the following structure :
+
+| name,start_id       |
+|---------------------|
+|Apis mellifera,0     |
+|Mantis religiosa,0   |
+|Martes americana,0   |
+
+An example of such a file is provided in the repository.
+
+The start ID is the ID of iNaturalist's observation from which you want to start downloading images. This is useful if something went wrong during a previous download and you want to restart from where it stopped. If you want to start downloading from the first species observation, just put '0' as start ID.
+
+2. Run the script : 
+
+```
+python inat_downloader.py [-o OBSERVATIONS] [-q QUALITY] [-l LICENSE] [-s SIZE] 
+``` 
+
+The script accepts the following arguments :
+
+`-o` `--observations` : Number of observations to download per species. Default : **_200_**  
+`-q` `--quality` : Quality of the observations (**_research_** or **_any_**). Default : **_research_**  
+`-l` `--license` : License(s) of the observations and photos to consider  (**_any_**, **_cc-by_**, **_cc-by-nc_**, **_cc-by-nd_**, **_cc-by-sa_**, **_cc-by-nc-nd_**, **_cc-by-nc-sa_**, **_cc0_**). Default : **_any_**  
+`-s` `--size` : Size of the images downloaded (**_small_**, **_medium_**, **_large_**, **_original_**). Default : **_medium_**  
+* Small :  maximum 240px on the longest side.
+* Medium : maximum 500px on the longest side.
+* Large : maximum 1024px on the longest side.
+* Original : maximum 2048px on the longest side.  
+
+
+**Example :**
+```
+python inat_downloader.py -o 5000 -q research -l cc-by,cc-by-nc,cc0 -s large
+```
+This would download, for each species, large images and metadata from 5000 research-grade observations. Only observations with cc-by, cc-by-nc and cc0 licenses will be considered.
+
+**Expected output :**
+
+In your terminal, you should see something like this : 
+
+``` 
+-------------------------- SCRIPT STARTED --------------------------
+
+INFO : Starting download of 5000 observations for Apis mellifera
+INFO : Apis mellifera - Observation 1/5000 (ID : 6093)
+INFO : 1 images downloaded (0.05 MB)
+INFO : 2 images downloaded (0.12 MB)
+INFO : 3 images downloaded (0.18 MB)
+INFO : Apis mellifera - Observation 2/5000 (ID : 7505)
+INFO : 4 images downloaded (0.57 MB)
+INFO : 5 images downloaded (1.01 MB)
+INFO : Apis mellifera - Observation 3/5000 (ID : 10000)
+INFO : 6 images downloaded (1.11 MB)
+INFO : 7 images downloaded (1.2 MB)
+```
+
+In a `results` folder, you should find for each species a folder containing the images downloaded and a CSV file containing the associated metadata : species_name, observation_id, observation_license, observer_login, observation_quality, observation_date, observation_latitude and observation_longitude.  
+Images are named as follow : `species_user_license_observation-id_photos-in-observation.jpg`. (e.g. `Apis-mellifera_Steve_cc-by-nc-nd_6093_3.jpg`)
+
+
+## Note
+
+The script relies on the iNaturalist API to download images and metadata. The API limits the request to 60 requests per hour. A delay between each request has been added to the script to comply with this limit. Additionally, the API restricts the downloads to 5 GB per hour and 24 GB per day. If these limits are nearly reached, the script will pause temporarily before resuming the download.
+
+## License 
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
---
+++ inat_downloader.py
@@ -0,0 +1,247 @@
+import argparse
+import csv
+import requests
+from requests.adapters import HTTPAdapter, Retry
+import os
+import datetime
+import time 
+
+# iNaturalist query limits, minus a safety margin 
+MAX_QUERIES_PER_DAY = 9500 # Max allowed : 10000
+MAX_MEDIA_PER_HOUR = 4 # Max allowed : 5 GB
+MAX_MEDIA_PER_DAY = 22 # Max allowed : 24 GB
+
+# User query information
+my_daily_queries = {"value" : 0, "reset_time" : datetime.datetime.now() + datetime.timedelta(hours = 24)}
+my_hourly_media = {"value" : 0, "reset_time" : datetime.datetime.now() + datetime.timedelta(hours = 1)}
+my_daily_media = {"value" : 0, "reset_time" : datetime.datetime.now() + datetime.timedelta(hours = 24)}
+
+# Run information
+max_observations_number = 0 
+current_observations_number = 0
+current_images_number = 0
+current_dataset_size = 0 
+
+# Set up a session that will retry on HTTP errors (429, 500, 502, 503, 504)
+session = requests.Session()
+retries = Retry(total = 5, backoff_factor = 1, status_forcelist = [429, 500, 502, 503, 504])
+session.mount("https://", HTTPAdapter(max_retries = retries))
+
+# Function to comply with iNaturalist query rate limits
+def evaluate_query_rate():
+
+    # Evaluate number of queries this day and wait if necessary
+    if my_daily_queries["value"] > MAX_QUERIES_PER_DAY:
+        while my_daily_queries["reset_time"] > datetime.datetime.now() :
+            time_left = my_daily_queries["reset_time"] - datetime.datetime.now()
+            print("WARNING : iNaturalist daily queries limit reached, download will resume in", str(time_left).split(".")[0], end = "\r")
+            time.sleep(1)
+        print()
+        my_daily_queries["value"] = 0
+        my_daily_queries["reset_time"] = datetime.datetime.now() + datetime.timedelta(hours = 24)
+    
+    # Evaluate size of media downloaded this hour and wait if necessary
+    if my_hourly_media["value"] > MAX_MEDIA_PER_HOUR:
+        while my_hourly_media["reset_time"] > datetime.datetime.now() :
+            time_left = my_hourly_media["reset_time"] - datetime.datetime.now()
+            print("WARNING : iNaturalist hourly media download limit reached, download will resume in", str(time_left).split(".")[0], end = "\r")
+            time.sleep(1)
+        print()
+        my_hourly_media["value"] = 0
+        my_hourly_media["reset_time"] = datetime.datetime.now() + datetime.timedelta(hours = 1)
+    
+    # Evaluate size of media downloaded this day and wait if necessary
+    if my_daily_media["value"] > MAX_MEDIA_PER_DAY:
+        while my_daily_media["reset_time"] > datetime.datetime.now() :
+            time_left = my_daily_media["reset_time"] - datetime.datetime.now()
+            print("WARNING : iNaturalist daily media download limit reached, download will resume in", str(time_left).split(".")[0], end = "\r")
+            time.sleep(1)
+        print()
+        my_daily_media["value"] = 0
+        my_daily_media["reset_time"] = datetime.datetime.now() + datetime.timedelta(hours = 24)
+
+# Function to download images and metadata from a set of observations
+def download(my_species_name, observations, image_size) :
+
+    global current_observations_number
+    global current_images_number
+    global current_dataset_size
+
+    # Run through all observations
+    for observation in observations:
+
+        # Get observation information
+        species_name = observation["taxon"]["name"]
+        observation_id = observation["id"]
+        observation_license = observation["license_code"]
+        if not observation_license :
+            observation_license = "none"
+        observer_login = observation["user"]["login"]
+        observation_quality = observation["quality_grade"]
+        observation_date = observation["observed_on"]
+        observation_latitude = observation["geojson"]["coordinates"][1]
+        if not observation_latitude :
+            observation_latitude = "none"
+        observation_longitude = observation["geojson"]["coordinates"][0]
+        if not observation_longitude :
+            observation_longitude = "none"
+
+        current_observations_number = current_observations_number + 1
+        print(f"INFO : {my_species_name} - Observation {current_observations_number}/{max_observations_number} (ID : {observation_id})")
+
+        # Write observation information in the CSV file
+        with open("results/" + my_species_name.replace(" ", "_") + "_metadata" + ".csv", 'a', newline = '') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow([
+                species_name,
+                observation_id,
+                observation_license,
+                observer_login,
+                observation_quality,
+                observation_date,
+                observation_latitude,
+                observation_longitude
+            ])
+
+        # Run through all images in the observation
+        for photo_id in range(len(observation["photos"])):
+
+            # Get image url and replace image size
+            image_url = observation["photos"][photo_id]["url"]
+            image_url = image_url.replace("/square", f"/{image_size}")
+            
+            # Create image file path
+            file_name = f"{species_name.replace(' ','-')}_{observer_login}_{observation_license}_{observation_id}_{photo_id}.jpeg"
+            file_path = os.path.join("results/" + my_species_name.replace(" ", "_") + "_images", file_name)
+
+            # Download image
+            image_response = session.get(image_url)
+            if image_response.status_code == 200:
+                with open(file_path, "wb") as image_file:
+                    image_file.write(image_response.content)
+                    current_images_number = current_images_number + 1
+                    current_dataset_size = current_dataset_size + len(image_response.content)/1000000
+                    print(f"INFO : {current_images_number} images downloaded ({round(current_dataset_size, 2)} MB)")
+
+                    # Update user query information
+                    my_daily_queries["value"] = my_daily_queries["value"] + 1
+                    my_hourly_media["value"] = my_hourly_media["value"] + len(image_response.content)/1000000000 
+                    my_daily_media["value"] = my_daily_media["value"] + len(image_response.content)/1000000000
+                    evaluate_query_rate()
+
+                    # Delay to avoid overloading the server
+                    time.sleep(1.2) 
+            else : 
+                print(f"WARNING : Couldn't download image at {image_url}")
+    return observation_id
+
+def main():
+
+    global max_observations_number
+    global current_observations_number
+    global current_images_number
+    global current_dataset_size
+
+    # Set configuration according to command line arguments
+    parser = argparse.ArgumentParser(description = "Download images from iNaturalist")
+    parser.add_argument("-o", "--observations", default = 200, type = int, help = "Number of observations to download per species")
+    parser.add_argument("-q", "--quality", default = "research", choices = ["research", "any"], help = "Observations quality grade (research or any)")
+    parser.add_argument("-s", "--size", default = "medium", choices = ["small", "medium", "large", "original"], help = "Images size (small, medium, large, original)")
+    parser.add_argument("-l", "--license", default = "any", help = "License(s) to consider (any, cc-by, cc-by-nc, cc-by-nc-nd, cc-by-nc-sa, cc-by-nd, cc-by-sa, cc0")
+    args = parser.parse_args()
+
+    print()
+    print("-------------------------- SCRIPT STARTED --------------------------")
+    print()
+
+    # Read CSV file with species names and start ids
+    my_species = []
+    with open("species.csv", "r") as species_file:
+        species_reader = csv.DictReader(species_file, delimiter = ",")
+        for row in species_reader:
+            if row["name"] not in [species["name"] for species in my_species] : # Avoid species duplication
+                my_species.append(row)
+
+    # Create a results folder 
+    os.makedirs("results", exist_ok=True)
+
+    # For each species, create a folder for images and a CSV file for observations metadata
+    for species in my_species:
+        images_folder = "results/" + species["name"].replace(" ", "_") + "_images"
+        os.makedirs(images_folder, exist_ok = True)
+        metadata_file = "results/" + species["name"].replace(" ", "_") + "_metadata" + ".csv"
+        with open(metadata_file, 'w', newline = '') as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow([
+            'species_name',
+            'observation_id',
+            'observation_license',
+            'observer_login',
+            'observation_quality',
+            'observation_date',
+            'observation_latidude',
+            'observation_longitude'
+        ])
+
+    # Run through all species 
+    for species in my_species:
+
+        # Reset run parameters 
+        current_observations_number = 0
+        current_images_number = 0
+        current_dataset_size = 0
+        id_above = species["start_id"]
+ 
+        # Get total number of observations available for the species
+        try :
+            response = session.get(f"https://api.inaturalist.org/v1/observations?"
+                                    f"taxon_name={species['name']}"
+                                    f"&quality_grade={args.quality}"
+                                    f"&has[]=photos"
+                                    f"&license={args.license}"
+                                    f"&photo_license={args.license}"
+                                    f"&page=1&per_page=1"
+                                    f"&order_by=id&order=asc&id_above={id_above}") 
+            if response.json()["total_results"] < args.observations :
+                print(f"WARNING : Only {response.json()['total_results']} observations available for {species['name']}")
+                print(f"INFO : Starting downloading of {response.json()['total_results']} observations for {species['name']}")
+                max_observations_number = response.json()["total_results"]
+            else :
+                print(f"INFO : Starting download of {args.observations} observations for {species['name']}")
+                max_observations_number = args.observations
+
+            # Fetch observations for the species
+            while current_observations_number < max_observations_number :
+                response = session.get(f"https://api.inaturalist.org/v1/observations?"
+                                        f"taxon_name={species['name']}"
+                                        f"&quality_grade={args.quality}"
+                                        f"&has[]=photos"
+                                        f"&license={args.license}"
+                                        f"&photo_license={args.license}"
+                                        f"&page=1&per_page={min(200, max_observations_number - current_observations_number)}"
+                                        f"&order_by=id&order=asc&id_above={id_above}") 
+
+                # Download images and metadata of the observations
+                observations = response.json()["results"]
+                id_above = download(species['name'], observations, args.size)
+
+            print(f"INFO : Images and metadata download for {species['name']} finished")
+        except requests.exceptions.RequestException :
+            print("ERROR : Connection error, please check your internet connection and try again")
+            print()
+            print("------------------- SCRIPT TERMINATED WITH ERROR -------------------")
+            print()
+            return
+        except FileNotFoundError as e :
+            print("ERROR : ", e.strerror, ":", e.filename)
+            print()
+            print("------------------- SCRIPT TERMINATED WITH ERROR -------------------")
+            print()
+            return
+
+    print()
+    print("------------------ SCRIPT TERMINATED SUCCESSFULLY ------------------")
+    print()
+
+if __name__ == "__main__":
+    main()
